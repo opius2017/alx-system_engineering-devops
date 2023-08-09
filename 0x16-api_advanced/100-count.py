091e7f7f500ccc63605aabfb618be2197bd68270
Owolabi Pius <opius2007@gmail.com>
2023-08-09 23:31:21 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-""" raddit api"""
-
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
-def count_words(subreddit, word_list, after=None, count=None):
-    if count is None:
-        count = {word.lower(): 0 for word in word_list}
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {'limit': 100}
-    if after:
-        params['after'] = after
-    
-    headers = {'User-Agent': 'bhalut'}
-    response = requests.get(url, params=params, headers=headers)
-    
-    if response.status_code == 200:
-        data = response.json()
-        topics = data['data']['children']
-        
-        for topic in topics:
-            title_words = topic['data']['title'].lower().split()
-            for word in title_words:
-                if word in count:
-                    count[word] += 1
-        
-        after = data['data']['after']
-        if not after:
-            sorted_words = sorted(count.keys(), key=lambda w: (-count[w], w))
-            
-            for word in sorted_words:
-                if count[word] > 0:
-                    print(f"{word}: {count[word]}")
-        else:
-            count_words(subreddit, word_list, after, count)
+
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        print(f"Error: {response.status_code}")
-
-# Example usage
-count_words('python', ['python', 'java', 'ruby', 'javascript'])
+        return (None)
